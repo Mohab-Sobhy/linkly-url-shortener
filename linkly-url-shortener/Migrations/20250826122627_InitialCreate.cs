021fc6557e49c03ca54187e9d441ef5c7c147678
Mohab-Sobhy <mohab_sobhy@outlook.com>
2025-08-26 19:23:09 +0300

    Migrations

---
+++ linkly-url-shortener/Migrations/20250826122627_InitialCreate.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace linkly_url_shortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuestUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SessionToken = table.Column<byte[]>(type: "bytea", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuestUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegisterUser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "bytea", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastLoginAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegisterUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Url",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RegisterUserId = table.Column<int>(type: "integer", nullable: true),
+                    GuestUserId = table.Column<int>(type: "integer", nullable: true),
+                    OriginalUrl = table.Column<string>(type: "text", nullable: false),
+                    ShortCode = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    IsCustomName = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Url", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Url_GuestUser_GuestUserId",
+                        column: x => x.GuestUserId,
+                        principalTable: "GuestUser",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Url_RegisterUser_RegisterUserId",
+                        column: x => x.RegisterUserId,
+                        principalTable: "RegisterUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UrlId = table.Column<int>(type: "integer", nullable: false),
+                    VisitedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IpAddress = table.Column<string>(type: "text", nullable: false),
+                    UserAgent = table.Column<string>(type: "text", nullable: true),
+                    Referer = table.Column<string>(type: "text", nullable: true),
+                    Country = table.Column<string>(type: "text", nullable: true),
+                    Browser = table.Column<string>(type: "text", nullable: true),
+                    OS = table.Column<string>(type: "text", nullable: true),
+                    DeviceType = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitLog_Url_UrlId",
+                        column: x => x.UrlId,
+                        principalTable: "Url",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Url_GuestUserId",
+                table: "Url",
+                column: "GuestUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Url_RegisterUserId",
+                table: "Url",
+                column: "RegisterUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitLog_UrlId",
+                table: "VisitLog",
+                column: "UrlId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VisitLog");
+
+            migrationBuilder.DropTable(
+                name: "Url");
+
+            migrationBuilder.DropTable(
+                name: "GuestUser");
+
+            migrationBuilder.DropTable(
+                name: "RegisterUser");
+        }
+    }
+}
---
+++ linkly-url-shortener/Migrations/20250826123635_DetermineDeleteTech.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linkly_url_shortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class DetermineDeleteTech : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Url_GuestUser_GuestUserId",
+                table: "Url");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Url_RegisterUser_RegisterUserId",
+                table: "Url");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Url_GuestUser_GuestUserId",
+                table: "Url",
+                column: "GuestUserId",
+                principalTable: "GuestUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Url_RegisterUser_RegisterUserId",
+                table: "Url",
+                column: "RegisterUserId",
+                principalTable: "RegisterUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Url_GuestUser_GuestUserId",
+                table: "Url");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Url_RegisterUser_RegisterUserId",
+                table: "Url");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Url_GuestUser_GuestUserId",
+                table: "Url",
+                column: "GuestUserId",
+                principalTable: "GuestUser",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Url_RegisterUser_RegisterUserId",
+                table: "Url",
+                column: "RegisterUserId",
+                principalTable: "RegisterUser",
+                principalColumn: "Id");
+        }
+    }
+}
